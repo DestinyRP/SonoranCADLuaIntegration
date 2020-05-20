8577c53132e8a52ee7278d71ee02ea579e1d11ba
Max Michaels <max32268@gmail.com>
2020-05-19 22:34:15 -0500

    disable loading of old unused file

--- sonoran_livemap/__resource.lua
+++ sonoran_livemap/__resource.lua
@@ -5,7 +5,6 @@ dependency "webpack"
 webpack_config "webpack.config.js"
 
 client_scripts{
-    "client/client.lua",
     "client/reverse_weapon_hashes.lua",
     "client/reverse_car_hashes.lua",
     "client/reverse_location_hashes.lua",
