48c3cc5aedb3f08c8f339235e67baf65f6023efe
Oliver <olipro@uptheinter.net>
2020-11-24 12:17:20 +0000

    Add DestinyRP config.js

---
+++ tablet/html/config.js
@@ -0,0 +1,3 @@
+$(function() {
+    document.getElementById('mdtFrame').src = "https://app.sonorancad.com/#/?comid=DESTINYRP&name=DestinyRP&subtitle=A%20serious%20FiveM%20roleplay%20server.&img=https://sonoran-cad.s3.amazonaws.com/2fe3937e-fc24-11ea-a9dd-0242ac130003/159cfccc-f903-11ea-adc9-0242ac130003/DestinyRP1.png&bkgLogin=0E0E0E&bkgLoginBox=191919"
+})
